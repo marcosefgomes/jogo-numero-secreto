648591aa0d8d4806918cd39dee330d440a3b98a3
marcosefgomes <m4rc05.g0m35@gmail.com>
2023-08-29 21:48:28 -0400

    game over function

--- app/validacao.js
+++ app/validacao.js
@@ -2,8 +2,18 @@ function verificaChuteValorValido(chute) {
   const numero = +chute;
 
   if (chuteInvalido(numero)) {
-    elementoChute.innerHTML += "<div>Valor inválido</div>";
-    return;
+    if (chute.toUpperCase() === "GAME OVER") {
+      document.body.innerHTML = `
+      <h2>Game Over!!!</h2>
+      <h3>Pressione o botão para jogar novamente</h3>
+  
+      <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+      `;
+      document.body.style.backgroundColor = "black";
+    } else {
+      elementoChute.innerHTML += "<div>Valor inválido</div>";
+      return;
+    }
   }
 
   if (numeroForaFaixa(numero)) {
